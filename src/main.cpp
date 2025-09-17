7cc15930a67c08de04667847cf406644f1731537
profile_2 <lombardofalcon@gmail.com>
2025-09-17 16:59:55 -0300

    Dailies

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #define OLC_PGE_APPLICATION
 #include "OneLoneCoder/olcPixelGameEngine.h"
+#include "profile_2/p2util.h"
 
 #define SCREEN_WIDTH    300
 #define SCREEN_HEIGHT   200
@@ -26,12 +27,12 @@ struct sPath{
         currentNode = 0;
     }
 
-    bool IsVertical(int node) { return node % 2 == 0; }
-    bool IsVertical() { return currentNode % 2 == 0;}
+    bool IsVertical(int node) { return node % 2; }
+    bool IsVertical() { return currentNode % 2; }
 
     olc::vf2d GetEnd(int node) { 
         if(node < nodes.size())
-            return IsVertical(node) ? olc::vf2d(nodes[node],0) : olc::vf2d(0,nodes[node]); 
+            return IsVertical(node) ? olc::vf2d(0,nodes[node]) : olc::vf2d(nodes[node],0); 
         else
             return olc::vf2d();
     }
@@ -49,9 +50,9 @@ struct sPath{
             }
             return coord;
         }
-        else if(node == nodes.size()){
-            return GetEndAbs(node);
-        }
+        // else if(node == nodes.size()){
+        //     return GetEndAbs(node);
+        // }
         else 
             return olc::vf2d();
     }
@@ -64,11 +65,28 @@ struct sPath{
             }
             return coord;
         }
-        if(node == nodes.size()){
-            return origin;
-        }
+        // if(node == nodes.size()){
+        //     return origin;
+        // }
         else
             return olc::vf2d();
+    }
+
+    void GetPoints(std::vector<olc::vf2d>& ends, const int& node){
+        ends.clear();
+        ends.emplace_back(GetStartAbs(node));
+        ends.emplace_back(GetEndAbs(node));
+    }
+    void GetPoints(std::vector<olc::vf2d>& ends){
+        GetPoints(ends, currentNode);
+    }
+
+    int GetStartExit(int node){
+
+    }
+
+    int GetEndExit(int node){
+        
     }
 
     void Draw(olc::PixelGameEngine& pge, int node, olc::Pixel color = olc::WHITE){
@@ -87,42 +105,89 @@ class cShip{
 private:
     olc::vf2d pos;
     int direction;
+    float clampT;
+    float clampB;
+    float clampL;
+    float clampR;
+
+    bool snapToLine;
+    float snapT;
+    float snapB;
+    float snapL;
+    float snapR;
 
 public:
-    cShip(olc::vf2d vfPos): pos(vfPos){
+    cShip(olc::vf2d vfPos, const float& fClampTop, const float& fClampBottom, const float& fClampLeft, const float& fClampRight): 
+    pos(vfPos), clampT(fClampTop), clampB(fClampBottom), clampL(fClampLeft), clampR(fClampRight){
         direction = DIR_UP;
+        snapToLine = false;
+    }
+
+    cShip(olc::vf2d vfPos): pos(vfPos){
+        cShip(vfPos, 0, SCREEN_HEIGHT, 0, SCREEN_WIDTH);
     }
 
     void SetDirection(int nDirection) { direction = nDirection; }
 
     void Move(int nDirection, float fSpeed){
-        switch(nDirection){
-            case DIR_UP:{
-                pos.y -= fSpeed;
-                break;
-            }
-            case DIR_DOWN:{
-                pos.y += fSpeed;
-                break;
-            }
-            case DIR_LEFT:{
-                pos.x -= fSpeed;
-                break;
-            }
-            case DIR_RIGHT:{
-                pos.x += fSpeed;
-                break;
-            }
+        float top;
+        float bottom;
+        float left;
+        float right;
+        if(!snapToLine){
+            top = clampT;
+            bottom = clampB;
+            left = clampL;
+            right = clampR;
         }
+        else{
+            top = snapT;
+            bottom = snapB;
+            left = snapL;
+            right = snapR;
+        }
+
+        switch(nDirection){
+            case DIR_UP:{ pos.y -= fSpeed; break; }
+            case DIR_DOWN:{ pos.y += fSpeed; break; }
+            case DIR_LEFT:{ pos.x -= fSpeed; break; }
+            case DIR_RIGHT:{ pos.x += fSpeed; break; }
+        }
+
+        if(pos.y < top) pos.y = top;
+        else if(pos.y > bottom) pos.y = bottom;
+        if(pos.x < left) pos.x = left;
+        else if(pos.x > right) pos.x = right;
+
         direction = nDirection;
     }
 
-    void MoveConstraing(){
+    void SnapToLine(const olc::vf2d& start, const olc::vf2d& end, const bool& verticalLine){
+        snapToLine = true;
+        if(verticalLine){
+            snapL = start.x;
+            snapR = start.x;
+            snapT = start.y < end.y ? start.y : end.y;
+            snapB = start.y > end.y ? start.y : end.y;
+            
+            pos.x = start.x;
+            if(pos.y < snapT) pos.y = snapT;
+            if(pos.y > snapB) pos.y = snapB;
+        }
+        else{
+            snapT = start.y;
+            snapB = start.y;
+            snapL = start.x < end.x ? start.x : end.x;
+            snapR = start.x > end.x ? start.x : end.x;
 
+            pos.y = start.y;
+            if(pos.x < snapL) pos.x = snapL;
+            if(pos.x > snapR) pos.x = snapR;
+        }
     }
 
-    void SnapToLine(){
-
+    void ReleaseFromLine(){
+        snapToLine = false;
     }
 
     void Draw(olc::PixelGameEngine& pge){
@@ -165,26 +230,43 @@ public:
         pge.DrawLine(pos, pos, color);
         pge.DrawTriangle(vertA,vertB,vertC,color);
     }
+
+    olc::vf2d GetPos() { return pos; }
 };
 
 class GAME : public olc::PixelGameEngine{
 private:
-    cShip ship = cShip(olc::vf2d(ScreenWidth()/2, ScreenHeight()/2));
-    sPath path = sPath(olc::vf2d(5,5));
+    const float fFieldMarginLeft = 5;
+    const float fFieldMarginRight = SCREEN_WIDTH - 5;
+    const float fFieldMarginTop = 5;
+    const float fFieldMarginBottom = SCREEN_HEIGHT - 5;
+
+    cShip ship = cShip(olc::vf2d(SCREEN_WIDTH/2, SCREEN_HEIGHT/2), fFieldMarginTop, fFieldMarginBottom, fFieldMarginLeft, fFieldMarginRight);
+    sPath path = sPath(olc::vf2d(fFieldMarginLeft, fFieldMarginTop));
 
 public:
     GAME(){
         sAppName = "QuiX";
     }
 
+    bool SnapShipToLine(int line){
+        ship.SnapToLine(path.GetStartAbs(line), path.GetEndAbs(line), path.IsVertical(line));
+        if (ship.GetPos() != olc::vf2d()) return true;
+        else return false;
+    }
+
     bool OnUserCreate(){
-        path.nodes.push_back(ScreenWidth()-10);
-        path.nodes.push_back(ScreenHeight()-10);
-        path.nodes.push_back(-ScreenWidth()+10);
+        path.nodes.push_back(fFieldMarginRight-fFieldMarginLeft);
+        path.nodes.push_back(fFieldMarginBottom-fFieldMarginTop);
+        path.nodes.push_back(-fFieldMarginRight+fFieldMarginLeft);
+        path.nodes.push_back(-fFieldMarginBottom+fFieldMarginTop);
+
+        SnapShipToLine(0);
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime){
+        Clear(olc::BLANK);
         int direction = DIR_UNDEFINED;
 
         if(GetKey(olc::Key::UP).bHeld) direction = DIR_UP;
@@ -193,12 +275,14 @@ public:
         else if(GetKey(olc::Key::RIGHT).bHeld) direction = DIR_RIGHT;
 
         if (direction != DIR_UNDEFINED){
+            if(GetKey(olc::Key::SPACE).bPressed)
+                ship.ReleaseFromLine();
             ship.Move(direction, fElapsedTime * 75);
         }
 
-        ship.Draw(*this);
         path.DrawAll(*this);
-        path.Draw(*this, 3, olc::GREEN);
+        path.Draw(*this, path.currentNode, olc::GREEN);
+        ship.Draw(*this);
         return true;
     }
 };
